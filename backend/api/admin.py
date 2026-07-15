adeb0b2ee5dfe8bfc1cee7043a1e0709e2c48363
Dmitry Remizov <rdk.7777@yahoo.com>
2026-07-15 21:46:16 +0300

    after pep 8

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,13 @@
+"""Admin configuration for the API application."""
+
 from django.contrib import admin
 
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
+    """Admin configuration for the Task model."""
+
     list_display = ('title', 'description', 'completed')
 
 
